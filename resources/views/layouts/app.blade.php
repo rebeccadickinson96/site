2f7428f088a7481af1ab2ec4418d7e12ba999872
Rebecca <rebecca@swipeandtap.co.uk>
2017-09-01 17:10:41 +0100

    Fix bootstrap functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,67 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ config('app.locale') }}">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.theme.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link href="/css/styles.css" rel="stylesheet">
     <link href="/datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lity/2.2.2/lity.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.4/summernote.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+
+    </script>
 </head>
 <body>
-<h1>Hello</h1>
-@yield('content');
+<div id="app">
+    <h1>Hello</h1>
+
+        @yield('content')
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script type="text/javascript" src="/datetimepicker/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+    @yield('vue-mixins')
+    @yield('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lity/2.2.2/lity.min.js"></script>
+    <script>
+        $(function () {
+            $(".datepicker").datepicker({
+                dateFormat: "dd/mm/yy"
+            });
+        });
+        $(document.location.hash).click();
+        window.setTimeout(function () {
+            $("#save-message").fadeTo(500, 0).slideUp(500, function () {
+                $(this).remove();
+            });
+        }, 2000);
+
+        $('.timepicker').datetimepicker({
+            format: 'dd/mm/yyyy hh:ii',
+            startView: 1,
+            minuteStep: 15,
+            autoclose: true
+        });
+    </script>
+</div>
 </body>
 </html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,35 +1,38 @@
 @extends('layouts.app')
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default panel-rbec">
-                <div class="panel-heading"><h2>Posts List</h2></div>
-                <div class="panel-body">
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Title</th>
-                            <th>Body</th>
-                            <th>Created At</th>
-                            <th>Action</th>
-                        </tr>
-                        @foreach($posts as $post)
-                            <tr>
-                                <td>{{ $post->title }}</td>
-                                <td>{{ $post->body }}</td>
-                                <td>{{ $post->created_at }}</td>
-                                <td><a class="btn btn-default pull-left" style="margin-right: 5px;"
-                                       href="/posts/{{ $post->id }}"><i
-                                                class="fa fa-eye"></i></a></td>
-                            </tr>
-                        @endforeach
-                    </table>
-                </div>
-                <div class="panel-footer">
-                    {{ $posts->render() }}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default panel-rbec">
+                    <div class="panel-heading"><h2>Posts List</h2></div>
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Body</th>
+                                    <th>Created At</th>
+                                    <th>Action</th>
+                                </tr>
+                                @foreach($posts as $post)
+                                    <tr>
+                                        <td>{{ $post->title }}</td>
+                                        <td>{{ $post->body }}</td>
+                                        <td>{{ $post->created_at }}</td>
+                                        <td><a class="btn btn-default pull-left" style="margin-right: 5px;"
+                                               href="/posts/{{ $post->id }}"><i
+                                                        class="fa fa-eye"></i></a></td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+
+                    </div>
+                    <div class="panel-footer">
+                        {{ $posts->render() }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
