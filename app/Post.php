ff8dc6bc0d7be97091ffcb2b2b632c1de2850bac
Rebecca <rebecca@swipeandtap.co.uk>
2017-09-08 12:26:35 +0100

    Add comments table and relationship

--- app/Post.php
+++ app/Post.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
+
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
 
