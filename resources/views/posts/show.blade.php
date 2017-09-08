ac15d98aeaae01a06cec48428b8141bb43522e09
Rebecca <rebecca@swipeandtap.co.uk>
2017-09-08 09:23:08 +0100

    Add title to posts.show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.app', ['title' => $post->title])
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1>{{ $post->title }}</h1>
-            </div>
-
-        </div>
-
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default panel-rbec">
                     <div class="panel-body">
                         {{ $post->body }}
