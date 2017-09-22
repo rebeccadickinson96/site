35d22cc8d73515ba81cfa75afd9beb283b2b7f72
Rebecca <rebecca@swipeandtap.co.uk>
2017-09-22 12:58:19 +0100

    Fix JavaScript template error

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,8 +7,6 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -18,5 +16,5 @@ window.Vue = require('vue');
 Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
-    el: '#app'
+    // el: '#app'
 });
