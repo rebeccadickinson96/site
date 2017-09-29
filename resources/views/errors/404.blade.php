f9ab13eab79c3d7837c2582944b7d69b7b19d22c
Rebecca <rebecca@swipeandtap.co.uk>
2017-09-29 14:33:36 +0100

    Add 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app',['title' => '404 Page Not Found!'])
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+
+                        <h3 class="text-center">Looks like this page does not exist.</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
