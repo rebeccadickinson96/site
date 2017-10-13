8cb55ebabbec4dbad38dae62ddf18481b6f834b8
Rebecca <rebecca@swipeandtap.co.uk>
2017-10-13 16:32:36 +0100

    Show months of posts in the sidebar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,12 @@ class HomeController extends Controller
     public function index()
     {
         $posts = Post::with('user')->where('date_published', '<' , Carbon::now())->orderBy('date_published', 'desc')->paginate($this->pagination);
+
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')
+            ->get()
+            ->toArray();
         $categories = Category::latest()->get();
-        return view('posts', compact('posts','categories','name','surname'));
+        return view('posts', compact('posts','categories', 'archives'));
     }
 }
