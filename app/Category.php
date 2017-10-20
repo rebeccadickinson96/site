646cbb34eea61cd193224b94b7dc483dae32a00d
Rebecca <rebecca@swipeandtap.co.uk>
2017-10-20 15:54:32 +0100

    Show tags in post list

--- app/Category.php
+++ app/Category.php
@@ -14,6 +14,6 @@ class Category extends Model
     }
 
     public static function allCategories(){
-        return static::latest()->get();
+        return static::whereHas('posts')->latest()->get();
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,12 +5,17 @@
             <div class="col-md-8 blog-main">
                 @foreach($posts as $post)
                     <div class="blog-post">
+
                         <div class="blog-post-title">
                             <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                         </div>
-                        <div class="blog-post-meta">{{ $post->date_published->diffForHumans()}} by <a>{{ $post->user->name }}</a></div>
+                        <div class="blog-post-meta">{{ $post->date_published->diffForHumans()}} by
+                            <a>{{ $post->user->name }}</a></div>
                         {{ $post->body }}
                         <div class="blog-post-meta">Comments:({{ $post->comments->count()}})</div>
+                        @foreach($post->categories->pluck('category') as $cat)
+                            {{ $cat }}
+                        @endforeach
 
                     </div>
                 @endforeach
