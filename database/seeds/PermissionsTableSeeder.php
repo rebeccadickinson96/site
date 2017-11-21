89c7a44909b0106571a0f4f77dfb735a87aa3399
Rebecca <rebecca@swipeandtap.co.uk>
2017-11-21 12:26:00 +0000

    Fix duplicate primary key in the permissions seeder

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -14,11 +14,11 @@ class PermissionsTableSeeder extends Seeder
     {
         DB::table('permissions')->insert([
             ['id' => 1, 'name' => 'manage-all-posts', 'display_name' => 'Manage All Posts', 'created_at' => Carbon::now()],
-            ['id' => 1, 'name' => 'manage-own-posts', 'display_name' => 'Manage Own Posts', 'created_at' => Carbon::now()],
-            ['id' => 2, 'name' => 'manage-categories', 'display_name' => 'Manage Categories', 'created_at' => Carbon::now()],
-            ['id' => 3, 'name' => 'delete-categories', 'display_name' => 'Delete Categories', 'created_at' => Carbon::now()],
-            ['id' => 4, 'name' => 'manage-users', 'display_name' => 'Manage Users', 'created_at' => Carbon::now()],
-            ['id' => 5, 'name' => 'manage-reports', 'display_name' => 'Manage Reports', 'created_at' => Carbon::now()],
+            ['id' => 2, 'name' => 'manage-own-posts', 'display_name' => 'Manage Own Posts', 'created_at' => Carbon::now()],
+            ['id' => 3, 'name' => 'manage-categories', 'display_name' => 'Manage Categories', 'created_at' => Carbon::now()],
+            ['id' => 4, 'name' => 'delete-categories', 'display_name' => 'Delete Categories', 'created_at' => Carbon::now()],
+            ['id' => 5, 'name' => 'manage-users', 'display_name' => 'Manage Users', 'created_at' => Carbon::now()],
+            ['id' => 6, 'name' => 'manage-reports', 'display_name' => 'Manage Reports', 'created_at' => Carbon::now()],
         ]);
     }
 }
